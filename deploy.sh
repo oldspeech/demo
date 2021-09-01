a18c0de8d6955188b56fe398481add3fda16cc21
oldspeech <alexandros.a.palaiologos@gmail.com>
2021-09-01 16:41:22 +0300

    modified deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t oldspeech/multi-client:latest -t cygnetops/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t oldspeech/multi-server:latest -t cygnetops/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t oldspeech/multi-worker:latest -t cygnetops/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push cygnetops/multi-client-k8s:latest
-docker push cygnetops/multi-server-k8s-pgfix:latest
-docker push cygnetops/multi-worker-k8s:latest
+docker push oldspeech/multi-client:latest
+docker push oldspeech/multi-server:latest
+docker push oldspeech/multi-worker:latest
 
-docker push cygnetops/multi-client-k8s:$SHA
-docker push cygnetops/multi-server-k8s-pgfix:$SHA
-docker push cygnetops/multi-worker-k8s:$SHA
+docker push oldspeech/multi-client:$SHA
+docker push oldspeech/multi-server:$SHA
+docker push oldspeech/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=oldspeech/multi-server:$SHA
+kubectl set image deployments/client-deployment client=oldspeech/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=oldspeech/multi-worker:$SHA
