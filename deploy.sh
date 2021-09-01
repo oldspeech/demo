0f71b0c27632088aac9e270212faf6fb81a8a9fa
oldspeech <alexandros.a.palaiologos@gmail.com>
2021-09-01 16:50:53 +0300

    deploy.sh without SHA env var

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t oldspeech/multi-client:latest -t cygnetops/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t oldspeech/multi-server:latest -t cygnetops/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t oldspeech/multi-worker:latest -t cygnetops/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t oldspeech/multi-client:latest -t cygnetops/multi-client -f ./client/Dockerfile ./client
+docker build -t oldspeech/multi-server:latest -t cygnetops/multi-server -f ./server/Dockerfile ./server
+docker build -t oldspeech/multi-worker:latest -t cygnetops/multi-worker -f ./worker/Dockerfile ./worker
 
 docker push oldspeech/multi-client:latest
 docker push oldspeech/multi-server:latest
 docker push oldspeech/multi-worker:latest
 
-docker push oldspeech/multi-client:$SHA
-docker push oldspeech/multi-server:$SHA
-docker push oldspeech/multi-worker:$SHA
+docker push oldspeech/multi-client
+docker push oldspeech/multi-server
+docker push oldspeech/multi-worker
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=oldspeech/multi-server:$SHA
-kubectl set image deployments/client-deployment client=oldspeech/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=oldspeech/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=oldspeech/multi-server
+kubectl set image deployments/client-deployment client=oldspeech/multi-client
+kubectl set image deployments/worker-deployment worker=oldspeech/multi-worker
